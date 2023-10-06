2f7a3a5c7e0a69de8518670368f21aa18588bbc0
walidsaber <walid_saber123@outlook.com>
2023-10-06 06:59:14 -0700

    100-textme.rb

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# script
+
+sender = ARGV[0].scan(/(?<=\[from:)[^ ]+(?=\])/).join
+reciever = ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
+flags = ARGV[0].scan(/(?<=\[flags:)[^ ]+(?=\])/).join
+puts "#{sender},#{reciever},#{flags}"
